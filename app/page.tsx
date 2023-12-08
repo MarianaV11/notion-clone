71f54055ddf35d30173d1b2911546ff4524da80d
Mariana Vieira <marianavca002@outlook.com>
2023-12-08 17:44:11 -0300

    Initializing project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Dessert } from "lucide-react";
+import { Button } from "./components/ui/button";
+
+export default function Home() {
+  return ( 
+      <div>
+        <Button variant="purple" size="default">Click me</Button>
+      </div>
+  )
+}
